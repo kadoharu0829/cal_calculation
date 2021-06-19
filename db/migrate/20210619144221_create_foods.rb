1fe3cb8cf727c5774eb79da4ab5e21ab97f8ba27
kadoharu0829 <kadoharu0829@gmail.com>
2021-06-20 01:36:45 +0900

    朝食を一覧画面に表示させるイメージまで

---
+++ db/migrate/20210619144221_create_foods.rb
@@ -0,0 +1,15 @@
+class CreateFoods < ActiveRecord::Migration[6.0]
+  def change
+    create_table :foods do |t|
+      t.string :name # 食品名
+      t.float :calorie # カロリー
+      t.float :protein # たんぱく質
+      t.float :lipid # 脂質
+      t.float :carbohydrate # 炭水化物
+      t.float :salt_equivalent # 食塩相当量
+      t.integer :kind # 区分
+
+      t.timestamps
+    end
+  end
+end
